3bb64645b24212182645f1eb1d9c2b2246ccc297
Ivan Belyavtsev <djbelyak@gmail.com>
2016-04-06 17:11:25 +0300

    Window is ready

--- DX11InputAssemblerStage/DX11InputAssemblerStage.cpp
+++ DX11InputAssemblerStage/DX11InputAssemblerStage.cpp
@@ -4,11 +4,85 @@
 #include "stdafx.h"
 
 
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
 int WINAPI WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine,
-                   int nCmdShow);
+                   int nShowCmd)
 {
-	return 0;
+    HWND hWnd;
+    WNDCLASSEX wc;
+
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpszClassName = L"WindowClass";
+
+    RegisterClassEx(&wc);
+
+    RECT wr = {0, 0, 800, 600};
+    AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+    hWnd = CreateWindowEx(NULL,
+                          L"WindowClass",
+                          L"DX11 Input-Assebler Stage Test",
+                          WS_OVERLAPPEDWINDOW,
+                          300,
+                          300,
+                          wr.right - wr.left,
+                          wr.bottom - wr.top,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+
+    ShowWindow(hWnd, nShowCmd);
+
+		//Init D3D
+
+		//Main loop
+		MSG msg;
+
+    while(TRUE)
+    {
+        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            if(msg.message == WM_QUIT)
+                break;
+        }
+        else
+        {
+            // Run game code here
+            // ...
+            // ...
+        }
+    }
+
+		//Clean
+
+    return msg.wParam;
 }
 
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch(message)
+    {
+        case WM_DESTROY:
+            {
+                PostQuitMessage(0);
+                return 0;
+            } break;
+    }
+
+    return DefWindowProc (hWnd, message, wParam, lParam);
+}
