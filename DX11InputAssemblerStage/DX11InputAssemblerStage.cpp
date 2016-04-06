519ec799f74712be88c69dd31aee1d1cbd296998
Ivan Belyavtsev <djbelyak@gmail.com>
2016-04-06 17:04:15 +0300

    https://github.com/djbelyak/DX11InputAssemblerStage.git

---
+++ DX11InputAssemblerStage/DX11InputAssemblerStage.cpp
@@ -0,0 +1,14 @@
+// DX11InputAssemblerStage.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow);
+{
+	return 0;
+}
+
